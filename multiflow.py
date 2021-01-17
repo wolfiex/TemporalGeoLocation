4ebec561c73fb1225b098485c2014b85571981f6
Daniel Ellis <daniel.ellis.research@gmail.com>
2021-01-17 23:52:11 +0000

    script

--- multiflow.py
+++ multiflow.py
@@ -6,10 +6,116 @@
 # In[1]:
 
 
+import re
+import numpy as np
+import pandas as pd
 
-r
-###########
+
+# In[2]:
+
+
+GPXfile='Lunch_Walk.gpx'
+data = open(GPXfile).read()
+
+
+# In[3]:
+
+
+lat = np.array(re.findall(r'lat="([^"]+)',data),dtype=float)
+lon = np.array(re.findall(r'lon="([^"]+)',data),dtype=float)
+time = re.findall(r'<time>([^\<]+)',data)
+dt = pd.to_datetime(time)
+s = dt.astype(int)
+
+coords = np.array(list(zip(lat,lon,time,dt,s)))
+coords = coords[::1]
+
+# In[4]:
+print ('coords')
+
+import gpflow
+import tensorflow as tf
+from gpflow.utilities import print_summary
+from gpflow.ci_utils import ci_niter
+# 
+plt.rcParams["figure.figsize"] = (12, 3)
+
+
+X = coords[:, -2].reshape(-1, 1)
+Xsec = coords[:, -1].reshape(-1, 1).astype(np.float64)
+
+Y0 = coords[:, 0].reshape(-1, 1).astype(np.float64)
+Y1 = coords[:, 1].reshape(-1, 1).astype(np.float64)
+
+Y=np.array(list(zip(Y0,Y1)))
+
+
+
+Yi = Y.astype(np.float64)
+Xi = coords[:, -1].reshape(-1, 1).astype(np.float64)
+
+second = 1000000000
+
+resolution = 0.0008
+timegap = 60*second
+
+Xarr = []
+Yarr = []
+Yold = [-999,-999]
+Xold = -999
+for j in range(len(Xi)):
+    i = Yi[j][0][0]
+    k = Yi[j][1][0]
+    if abs(i-Yold[0])>resolution:
+        Yarr.append([i,k])
+        Yold=[i,k]
+        Xold = Xi[j]
+        Xarr.append(Xi[j])
+    elif abs(Xi[j]-Xold)>timegap:
+        Yarr.append([i,k])
+        Yold=[i,k]
+        Xold = Xi[j]
+        Xarr.append(Xi[j])
+    elif abs(k-Yold[1])>resolution:
+        Yarr.append([i,k])
+        Yold=[i,k]
+        Xold = Xi[j]
+        Xarr.append(Xi[j])
+ 
+
+
+Xarr= np.array(Xarr).reshape(-1, 1).astype(np.float64)
+Yarr= np.array(Yarr)#.reshape(-1, 1).astype(np.float64)
+
+xx = [Xarr.min()]
+# Xsec = (Xsec - Xsec.mean()) / Xsec.std()
+Xarr -= Xarr.min()
+xx.append(Xarr.max())
+Xarr /= Xarr.max()
+
+
+
+means=[]
+scales = []
+for q in [0,1]:
+
+    scale = 1/Yarr[:,q].std()
+    Yarr[:,q]=Yarr[:,q]*scale #1/resolution
+    scales.append(scale)
+    
+    Ym = Yarr[:,q].mean()
+    Yarr[:,q] = Yarr[:,q]-Ym
+    means.append(Ym)
+    
+
+
+X=Xarr
+Y=Yarr
+
+##########
 #data
+
+
 ###########
 MAXITER = ci_niter(2000)
 D = 1  # number of input dimensions
@@ -40,6 +146,9 @@ m = gpflow.models.SVGP(kernel, gpflow.likelihoods.GaussianMC(), inducing_variabl
 print_summary(m)
 
 
+# In[91]:
+
+
 kernel =  gpflow.kernels.Matern52( variance=1, lengthscales=1.2)+ gpflow.kernels.White() #+gpflow.kernels.Exponential() 
 
 m = gpflow.models.GPR(data=(Xarr, Yarr), kernel=kernel, mean_function=None)
@@ -52,10 +161,6 @@ opt_logs = opt.minimize(m.training_loss, m.trainable_variables, options=dict(max
 print_summary(m)
 
 
-# In[92]:
-
-
-Y.shape,X.shape
 
 
 # In[93]:
@@ -110,6 +215,8 @@ tf.saved_model.save(m, save_dir)
 
 l = tf.saved_model.load(save_dir)
 l.predict_f_compiled(np.array([[0.2]]))
+
+
 
 
 
